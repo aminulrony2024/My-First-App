2495e98041e52ef71ab8dabbd5d52c00402285f6
aminulrony2024 <aminulrony2024@gmail.com>
2024-09-21 11:36:23 +0600

    toast and context added

--- app/src/main/java/com/aminulrony/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/aminulrony/myfirstapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.aminulrony.myfirstapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
@@ -13,6 +14,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.aminulrony.myfirstapp.ui.theme.MyFirstAppTheme
 
@@ -38,7 +40,8 @@ fun Aminul(){
         Text("Text is working")
         OutlinedTextField(value = "outlined text field", onValueChange = {})
         Row {
-            Button(onClick = {}) {
+            val context = LocalContext.current
+            Button(onClick = {Toast.makeText(context,"Button is clicked", Toast.LENGTH_LONG)}) {
                 Text("Submit")
             }
         }
