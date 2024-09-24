2fd00d87255f5cee3c2ba2aa2055dc8e5b257d5d
aminulrony2024 <aminulrony2024@gmail.com>
2024-09-24 17:15:55 +0600

    box is added

--- app/src/main/java/com/aminulrony/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/aminulrony/myfirstapp/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.aminulrony.myfirstapp
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.aminulrony.myfirstapp.ui.theme.MyFirstAppTheme
 
@@ -39,10 +37,9 @@ fun Aminul(){
     Column {
         Text("Text is working")
         OutlinedTextField(value = "outlined text field", onValueChange = {})
-        Row {
-            val context = LocalContext.current
-            Button(onClick = {Toast.makeText(context,"Button is clicked", Toast.LENGTH_LONG).show()}) {
-                Text("Submit")
+        Row{
+            Box {
+
             }
         }
     }
