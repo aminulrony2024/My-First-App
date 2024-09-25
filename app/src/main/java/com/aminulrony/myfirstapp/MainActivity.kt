f3587be2c5422a27f88376941d0534bb058f7496
aminulrony2024 <aminulrony2024@gmail.com>
2024-09-25 10:50:13 +0600

    spacer is added

--- app/src/main/java/com/aminulrony/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/aminulrony/myfirstapp/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
@@ -20,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.aminulrony.myfirstapp.ui.theme.MyFirstAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -46,7 +49,9 @@ fun Aminul(){
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Text is working")
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "outlined text field", onValueChange = {})
+        Spacer(modifier = Modifier.height(16.dp))
         Row{
             Box {
                 Button(onClick = {}) {
